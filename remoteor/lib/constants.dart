e1ff70620e6e63b3ce071c0acaa981b632d1fbb7
Prakash251299 <88026044+Prakash251299@users.noreply.github.com>
2025-01-29 20:32:49 +0530

    Update constants.dart

--- remoteor/lib/constants.dart
+++ remoteor/lib/constants.dart
@@ -1,8 +1,8 @@
 int local_port = 8000;
 int remote_port = 9090;
-String host = "98.70.55.107";
-String username = "azureuser";
-String password = "@Ishu251299p7485037390";
+String host = "";
+String username = "";
+String password = "";
 
 enum LoadPage {
   loading,
